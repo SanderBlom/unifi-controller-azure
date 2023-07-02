5cbffb96030418ac436ce8108272154f61ebd3c0
Sander Blomvågnes <sander@blinq.n>
2023-07-02 15:42:18 +0200

    Fix issue with name

--- storage_share.tf
+++ storage_share.tf
@@ -1,5 +1,5 @@
 resource "azurerm_storage_share" "unifi_share" {
-  name                 = "unifi_controller"
+  name                 = var.project_name
   storage_account_name = azurerm_storage_account.unifi_sa.name
   quota                = 50
 }
