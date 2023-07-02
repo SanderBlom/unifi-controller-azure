1ce218f634a0b35cf8e3dd493534eadd71643840
Sander Blomvågnes <sander@blinq.n>
2023-07-02 16:01:40 +0200

    Removed port to match 5 port limit

--- container_group.tf
+++ container_group.tf
@@ -12,82 +12,29 @@ resource "azurerm_container_group" "example" {
     cpu    = 1
     memory = 2
 
-    # Used for DNS. This is required for Guest Portal redirection, downloading updates, and remote access.
-    ports {
-      port     = 53
-      protocol = "TCP"
-    }
-
-    # Used for STUN.
     ports {
       port     = 3478
       protocol = "UDP"
     }
 
-    # Used for remote syslog capture.
-    ports {
-      port     = 5514
-      protocol = "UDP"
-    }
-
-    # Used for device and application communication.
-    ports {
-      port     = 8080
-      protocol = "TCP"
-    }
-
-    # Used for application GUI/API as seen in a web browser.
-    ports {
-      port     = 443
-      protocol = "TCP"
-    }
-
-    # Used for application GUI/API as seen in a web browser.
-    ports {
-      port     = 8443
-      protocol = "TCP"
-    }
-
-    # Used for HTTP portal redirection.
-    ports {
-      port     = 8880
-      protocol = "TCP"
-    }
-
-    # Used for HTTPS portal redirection.
-    ports {
-      port     = 8843
-      protocol = "TCP"
-    }
-
-    # Used for UniFi mobile speed test.
-    ports {
-      port     = 6789
-      protocol = "TCP"
-    }
-
-    # Used for local-bound database communication.
-    ports {
-      port     = 27117
-      protocol = "TCP"
-    }
-
-    # Used for device discovery.
     ports {
       port     = 10001
       protocol = "UDP"
     }
 
-    # Used to "Make application discoverable on L2 network" in the UniFi Network settings.
     ports {
-      port     = 1900
-      protocol = "UDP"
+      port     = 8080
+      protocol = "TCP"
     }
 
-    # Used for NTP (date and time). Required for establishing secure communication with remote access servers.
     ports {
-      port     = 123
-      protocol = "UDP"
+      port     = 8443
+      protocol = "TCP"
+    }
+
+    ports {
+      port     = 8880
+      protocol = "TCP"
     }
 
     volume {
